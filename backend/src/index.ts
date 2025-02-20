0a8df899e72bbe918c5b1d8c6e8272fe9b96c95a
jgonzal10 <jgonzal10@gmail.com>
2025-02-20 09:11:49 +0100

    logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
 app.use(express.json());
+app.use(errorHandler);
 
 export default app;
