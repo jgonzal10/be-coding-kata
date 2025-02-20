22d72130602672057ac6d72f5002c86b9c9eeb13
jgonzal10 <jgonzal10@gmail.com>
2025-02-20 09:14:14 +0100

    adding logs to controller

--- backend/src/controllers/total.controller.ts
+++ backend/src/controllers/total.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { ProductQuantity } from "../models/productQuantity";
 import { calculateProductsQuantity } from "../utils/calculateProductsQuantity";
 import { calculateTotalPricePerProduct } from "../utils/calculateTotalPricePerProduct";
+import { logger } from "../utils/logger";
 
 export const getTotal = (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -14,9 +15,10 @@ export const getTotal = (req: Request, res: Response, next: NextFunction) => {
         total =
           total + calculateTotalPricePerProduct(prod.quantity!, prod.product);
       });
-  
+      logger.info("Successfully total calculated");
       res.status(200).json({ total });
     }else{
+      logger.warn("No products to calculate");
       res.status(200).json({ total:0 });
     }
 
